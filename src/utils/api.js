22f29e194a897f308f3ba168a8438dc603c4e99a
devalencia12 <devalencia@bancodeoccidente.com.co>
2022-12-19 14:22:42 -0500

    :sparkles: Last changes with images queries and pagination

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyBIeoIFLyTtZmE2y6UGCE_645TQOg_vdTo",
+  cx: "e62f5813ba9f047c7",
+};
+
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
